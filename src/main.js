d91660ea44adbd463b21c9a5a04e520099e0c8d8
xyl <https://github.com/xyl97/97.git>
2023-08-20 07:49:34 +0800

    前端拦截

--- src/main.js
+++ src/main.js
@@ -20,18 +20,29 @@ axios.defaults.withCredentials = true
 
 axios.interceptors.request.use(function (config) {
 
-  console.log(config);
+
+
   return config;
 });
 
-//响应拦截器
-// axios.interceptors.response.use(function (success) {
-//     console.log(success.data.msg)
-//   if (success.data.msg==="请重新登录") {
-//         router.replace("/login")
-//     return response;
-//   }
-// })
+// 添加响应拦截器
+axios.interceptors.response.use(function (response) {
+
+
+  if (response.data.msg=="请重新登录"){
+
+
+    router.replace("/")
+
+  }
+  // 2xx 范围内的状态码都会触发该函数。
+  // 对响应数据做点什么
+  return response;
+}, function (error) {
+  // 超出 2xx 范围的状态码都会触发该函数。
+  // 对响应错误做点什么
+  return Promise.reject(error);
+});
 
 
 
