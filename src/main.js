9c5eb346a57d9a08630b3c8f3e68a20d78c2051f
xyl <https://github.com/xyl97/97.git>
2023-08-20 08:01:02 +0800

    main

--- src/main.js
+++ src/main.js
@@ -31,16 +31,13 @@ axios.interceptors.response.use(function (response) {
 
   if (response.data.msg=="请重新登录"){
 
-
     router.replace("/")
 
   }
-  // 2xx 范围内的状态码都会触发该函数。
-  // 对响应数据做点什么
+
   return response;
 }, function (error) {
-  // 超出 2xx 范围的状态码都会触发该函数。
-  // 对响应错误做点什么
+
   return Promise.reject(error);
 });
 
